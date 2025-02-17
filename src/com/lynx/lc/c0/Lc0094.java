337b738972b675210c58da893423ac1608feaeb6
skyLiyx <ysing321@outlook.com>
2025-02-17 20:33:02 +0800

    Binary tree traversal algorithms.

---
+++ src/com/lynx/lc/c0/Lc0094.java
@@ -0,0 +1,29 @@
+package com.lynx.lc.c0;
+
+import com.lynx.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 94.二叉树的中序遍历
+ */
+public class Lc0094 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        // 将所有左边界压入栈，每弹出一个节点，再次压入该节点右子节点的左边界
+        while (!stack.isEmpty() || root != null) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                TreeNode cur = stack.pop();
+                res.add(cur.val);
+                root = cur.right;
+            }
+        }
+        return res;
+    }
+}
---
+++ src/com/lynx/lc/c1/Lc0144.java
@@ -0,0 +1,32 @@
+package com.lynx.lc.c1;
+
+import com.lynx.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 144.二叉树的前序遍历
+ */
+public class Lc0144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            res.add(cur.val);
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+        }
+        return res;
+    }
+}
