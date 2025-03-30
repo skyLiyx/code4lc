201696d1299e797809c2188c2faa2ce2b52a3db8
skyLiyx <ysing321@outlook.com>
2025-03-30 16:42:56 +0800

    some practices.

--- src/com/lynx/lc/c0/Lc0010.java
+++ src/com/lynx/lc/c0/Lc0010.java
@@ -4,60 +4,76 @@ package com.lynx.lc.c0;
  * 10.正则表达式匹配
  *
  * @apiNote
- * <b>case 1</b>: {@code p.charAt(j) == s.charAt(i)}
+ * 用两个指针 i, j 分别指向字符串 s 开头和模式串 p 开头, 根据匹配情况向后移动
  * <pre>
- *     ######a(i)
- *     ####a(j)
- *  </pre>
- * <b>case 2</b>: if {@code p.charAt(j) == '.'}
- * <pre>
- *     #######a(i)
- *     ####.(j)
- *  </pre>
- * <b>case 3</b>: if {@code p.charAt(j) == '*'}<p>
- * 3.1 if {@code p.charAt(j - 1) != '.' && p.charAt(j - 1) != s.charAt(i)},
- * then b* is counted as empty.
- * <pre>
- *     #####a(i)
- *     ####b*(j)
- * </pre>
- * 3.2 if {@code p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i)}:
- * <pre>
- *     ######a(i)
- *     ####.*(j)
+ * - 如果 j+1 指向字符不是 '*'
+ *   两个指针指向的字符就必须相同，要么是两个相同的字符，要么p对应的是 '.'.
  *
- *     #####a(i)
- *     ###a*(j)
- * </pre>
- * there are also 3 cases:</br>
- *     - if {@code p.charAt(j - 1)} is counted as empty, then {@code dp[i][j] = dp[i][j - 2]}</br>
- *     - if counted as one, then {@code dp[i][j] = dp[i - 1][j - 2]}</br>
- *     - if counted as multiple, then {@code dp[i][j] = dp[i - 1][j]}</br>
+ * - 如果 j+1 指向字符是 '*'
+ *   j 和 j+1 组成 '_*' 作为整体考虑, 分两种情况：
+ *   - 匹配0个 s 中的字符，指针 i 不移动, j 右移2
+ *   - 匹配1个 s 中的字符，指针 i 右移1, j 不移动
  *
+ * - 如果 i 越界
+ *   - j 也越界, 说明整体匹配成功
+ *   - j 没越界, 如果 j+1 也没越界且是 '*', 可以消除掉 j 和 j+1, 后续继续比较
+ *
+ * - 如果 j 越界, 但是 i 没越界, 匹配失败
+ * </pre>
  */
 public class Lc0010 {
     public boolean isMatch(String s, String p) {
         int m = s.length(), n = p.length();
+        char[] c1 = s.toCharArray();
+        char[] c2 = p.toCharArray();
         boolean[][] dp = new boolean[m + 1][n + 1];
-        dp[0][0] = true;
-        for (int j = 2; j <= n; j += 2) {
-            if (p.charAt(j - 1) == '*' && dp[0][j - 2]) {
-                dp[0][j] = true;
-            }
+        dp[m][n] = true;
+        for (int j = n - 2; j >= 0; j--) {
+            dp[m][j] = c2[j + 1] == '*' && dp[m][j + 2];
         }
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else if (p.charAt(j - 1) == '*') {
-                    if (p.charAt(j - 2) != '.' && p.charAt(j - 2) != s.charAt(i - 1)) {
-                        dp[i][j] = dp[i][j - 2];
-                    } else {
-                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1] || dp[i][j - 2];
-                    }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (j + 1 == n || c2[j + 1] != '*') {
+                    dp[i][j] = (c1[i] == c2[j] || c2[j] == '.') && dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = dp[i][j + 2] || ((c1[i] == c2[j] || c2[j] == '.') && dp[i + 1][j]);
                 }
             }
         }
-        return dp[m][n];
+        return dp[0][0];
+    }
+
+    /**
+     * 暴力递归版本
+     */
+    private boolean f(char[] s, int i, char[] p, int j) {
+        if (i == s.length) {
+            // s匹配完了
+            if (j == p.length) {
+                // p也匹配完了
+                return true;
+            } else {
+                // p还剩一些后缀
+                // 后续的_*可以被消灭掉
+                return j + 1 < p.length && p[j + 1] == '*' && f(s, i, p, j + 2);
+            }
+        } else if (j == p.length) {
+            // s没匹配完，p匹配完了
+            return false;
+        } else {
+            // s和p都没匹配完
+            if (j + 1 == p.length || p[j + 1] != '*') {
+                // p后面第二个不是 '*'
+                // 那么必须单独匹配这一个字符
+                return (s[i] == p[j] || p[j] == '.') && f(s, i + 1, p, j + 1);
+            } else {
+                // p后面第二个是 '*'
+                // 要么跳过这个 _*
+                boolean p1 = f(s, i, p, j + 2);
+                // 要么不跳过，匹配一个s
+                boolean p2 = (s[i] == p[j] || p[j] == '.') && f(s, i + 1, p, j);
+                return p1 || p2;
+            }
+        }
     }
 }
--- src/com/lynx/lc/c0/Lc0044.java
+++ src/com/lynx/lc/c0/Lc0044.java
@@ -2,67 +2,83 @@ package com.lynx.lc.c0;
 
 /**
  * 44.通配符匹配
+ *
+ * @see Lc0010
  */
 public class Lc0044 {
     public boolean isMatch(String s, String p) {
-        if (s.isEmpty()) {
-            return p.isEmpty() || allStars(p);
+        if (allStars(p.toCharArray())) {
+            return true;
         }
-        if (p.isEmpty()) {
-            return false;
-        }
-        p = removeDupStars(p);
-        int m = s.length(), n = p.length();
+        p = removeDuplicateStars(p.toCharArray());
+        return isMatch(s.toCharArray(), p.toCharArray());
+    }
+
+    private boolean isMatch(char[] s, char[] p) {
+        int m = s.length;
+        int n = p.length;
         boolean[][] dp = new boolean[m + 1][n + 1];
         dp[m][n] = true;
-        dp[m][n - 1] = p.charAt(n - 1) == '*';
+        for (int j = n - 1; j >= 0; j--) {
+            dp[m][j] = p[j] == '*' && dp[m][j + 1];
+        }
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                if (p.charAt(j) == '?') {
-                    dp[i][j] = dp[i + 1][j + 1];
-                } else if (p.charAt(j) >= 'a' && p.charAt(j) <= 'z') {
-                    dp[i][j] = s.charAt(i) == p.charAt(j) && dp[i + 1][j + 1];
-                } else if (j == n - 1) {
-                    dp[i][j] = true;
+                if (p[j] != '*') {
+                    dp[i][j] = (p[j] == '?' || s[i] == p[j]) && dp[i + 1][j + 1];
                 } else {
-                    int k = i;
-                    while (k < s.length()) {
-                        if (dp[k][j + 1]) {
-                            dp[i][j] = true;
-                            break;
-                        }
-                        k++;
-                    }
+                    dp[i][j] = dp[i][j + 1] || dp[i + 1][j];
                 }
             }
         }
         return dp[0][0];
     }
 
-    private boolean allStars(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != '*') {
+    /**
+     * 暴力递归版本
+     */
+    private boolean f(char[] s, int i, char[] p, int j) {
+        if (i == s.length) {
+            if (j == p.length) {
+                return true;
+            } else {
+                return p[j] == '*' && f(s, i, p, j + 1);
+            }
+        } else if (j == p.length) {
+            return false;
+        } else {
+            if (p[j] != '*') {
+                return (p[j] == '?' || s[i] == p[j]) && f(s, i + 1, p, j + 1);
+            } else {
+                return f(s, i, p, j + 1) || f(s, i + 1, p, j);
+            }
+        }
+    }
+
+    private boolean allStars(char[] p) {
+        for (char c : p) {
+            if (c != '*') {
                 return false;
             }
         }
-        return true;
+        return p.length != 0;
     }
 
     /**
      * 去除模式串中重复的星号。
      */
-    private String removeDupStars(String p) {
+    private String removeDuplicateStars(char[] p) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < p.length(); i++) {
-            if (p.charAt(i) == '*' && sb.length() != 0 && sb.charAt(sb.length() - 1) == '*') {
+        for (int i = 0; i < p.length; i++) {
+            if (p[i] == '*' && i - 1 >= 0 && p[i - 1] == '*') {
                 continue;
             }
-            sb.append(p.charAt(i));
+            sb.append(p[i]);
         }
         return sb.toString();
     }
 
-    public boolean isMatch_v1(String s, String p) {
+    public boolean isMatch1(String s, String p) {
         int i = 0, j = 0, iStar = -1, jStar = -1, m = s.length(), n = p.length();
         while (i < m) {
             if (j < n && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')) {
